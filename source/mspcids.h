494632479186f3a9c73f27b4949e04edf3a3a281
Hamartia666 <nikoklisch@gmail.com>
2024-03-22 21:50:36 +0000

    Create the initial source files
    These files are created by the VST3_Generator software, as a fundamental
    basis of which to make the custom plugin

---
+++ source/mspcids.h
@@ -0,0 +1,18 @@
+//------------------------------------------------------------------------
+// Copyright(c) 2024 HonoursProject.
+//------------------------------------------------------------------------
+
+#pragma once
+
+#include "pluginterfaces/base/funknown.h"
+#include "pluginterfaces/vst/vsttypes.h"
+
+namespace HonoursProject {
+//------------------------------------------------------------------------
+static const Steinberg::FUID kMultiStagePhaserProcessorUID (0xB7059340, 0x298756BB, 0x9553DCDB, 0x2EE72454);
+static const Steinberg::FUID kMultiStagePhaserControllerUID (0x47AD0A4B, 0xB6E05EA5, 0xA891342B, 0x63091CD6);
+
+#define MultiStagePhaserVST3Category "Fx"
+
+//------------------------------------------------------------------------
+} // namespace HonoursProject
